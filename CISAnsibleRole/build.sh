247ab3c4e36b2fc487153677db262444f28c9556
Ryan Timmons <rtimmons@betterment.com>
2017-07-20 18:49:48 -0400

    ulimit and empty test

--- CISAnsibleRole/build.sh
+++ CISAnsibleRole/build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 mkdir -p build/{root,test}
+touch build/root/test/nop # workaround for test-free packages for now
 
 pkg() {
     mkdir -p "build/root/ansible/roles/cis-amazon-linux"
-    cp -R ../{defaults,files,handlers,meta,tasks,vars} ./build/root/ansible/roles/cis-amazon-linux/
+    for D in defaults files handlers meta tasks vars; do
+        cp -R "../$D" ./build/root/ansible/roles/cis-amazon-linux/
+    done
 }
 
 
